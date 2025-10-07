fe8e762af51b165eab0fba9682df1d4e0df55070
Rtur2003 <hasannarthurrr@gmail.com>
2025-10-07 17:41:59 +0300

    .

--- platform/pages/_document.tsx
+++ platform/pages/_document.tsx
@@ -4,21 +4,32 @@ export default function Document() {
   return (
     <Html lang="tr">
       <Head>
-        {/* Favicon ve Icons */}
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="alternate icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-
-        {/* Manifest */}
-        <link rel="manifest" href="/manifest.json" />
-
-        {/* Meta Tags */}
-        <meta name="theme-color" content="#00D4FF" />
-        <meta name="msapplication-TileColor" content="#000000" />
-
-        {/* Preconnect */}
+        {/* DNS Prefetch & Preconnect for Performance */}
+        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+
+        {/* Favicon & Icons - Multiple sizes */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="alternate icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
+        {/* PWA Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme Colors - Brand identity */}
+        <meta name="theme-color" content="#fbbf24" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#0f0f0f" media="(prefers-color-scheme: dark)" />
+        <meta name="msapplication-TileColor" content="#fbbf24" />
+
+        {/* Apple Mobile Web App */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="CrownCode" />
+
+        {/* Security & Performance */}
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="x-dns-prefetch-control" content="on" />
       </Head>
       <body>
         <Main />
