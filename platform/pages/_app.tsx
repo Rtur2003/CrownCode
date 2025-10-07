863c6862a2ad4b2491f42e872fe3cd15c0854441
Rtur2003 <hasannarthurrr@gmail.com>
2025-10-07 16:35:05 +0300

    ,,

--- platform/pages/_app.tsx
+++ platform/pages/_app.tsx
@@ -1,10 +1,10 @@
 /**
  * Next.js App Component
  * Kullanım: Tüm sayfalar için global wrapper
- * Bağımlılıklar: ThemeProvider, LanguageProvider, ToastProvider, ErrorBoundary, SearchModal, global styles
+ * Bağımlılıklar: ThemeProvider, LanguageProvider, ToastProvider, ErrorBoundary, LoadingScreen, global styles
  */
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { LanguageProvider } from '@/context/LanguageContext'
@@ -13,9 +13,25 @@ import { ToastContainer } from '@/components/UI/Toast/ToastContainer'
 import { ErrorBoundary } from '@/components/ErrorBoundary/ErrorBoundary'
 import { ShortcutsModal } from '@/components/KeyboardShortcuts/ShortcutsModal'
 import { SearchModal } from '@/components/Search/SearchModal'
+import { LoadingScreen } from '@/components/Loading/LoadingScreen'
 import '@/styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    // Check if this is first load
+    const hasLoaded = sessionStorage.getItem('hasLoaded')
+    if (hasLoaded) {
+      setIsLoading(false)
+    }
+  }, [])
+
+  const handleLoadingComplete = () => {
+    sessionStorage.setItem('hasLoaded', 'true')
+    setIsLoading(false)
+  }
+
   return (
     <ErrorBoundary>
       <ThemeProvider
@@ -26,6 +42,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       >
         <LanguageProvider>
           <ToastProvider>
+            {isLoading && <LoadingScreen onLoadingComplete={handleLoadingComplete} />}
             <Component {...pageProps} />
             <ToastContainer />
             <ShortcutsModal />
