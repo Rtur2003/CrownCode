de33b7d9c71d0868c40da490d6b3aa049e5fe8c9
Rtur2003 <hasannarthurrr@gmail.com>
2025-10-07 16:15:12 +0300

    :)

--- platform/pages/_app.tsx
+++ platform/pages/_app.tsx
@@ -11,6 +11,7 @@ import { LanguageProvider } from '@/context/LanguageContext'
 import { ToastProvider } from '@/context/ToastContext'
 import { ToastContainer } from '@/components/UI/Toast/ToastContainer'
 import { ErrorBoundary } from '@/components/ErrorBoundary/ErrorBoundary'
+import { ShortcutsModal } from '@/components/KeyboardShortcuts/ShortcutsModal'
 import '@/styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -26,6 +27,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           <ToastProvider>
             <Component {...pageProps} />
             <ToastContainer />
+            <ShortcutsModal />
           </ToastProvider>
         </LanguageProvider>
       </ThemeProvider>
