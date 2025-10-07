818682a1a85a4614cf7baf97127a802fc0cb5843
Rtur2003 <hasannarthurrr@gmail.com>
2025-10-07 16:24:47 +0300

    :)

--- platform/pages/_app.tsx
+++ platform/pages/_app.tsx
@@ -1,7 +1,7 @@
 /**
  * Next.js App Component
  * Kullanım: Tüm sayfalar için global wrapper
- * Bağımlılıklar: ThemeProvider, LanguageProvider, ToastProvider, ErrorBoundary, global styles
+ * Bağımlılıklar: ThemeProvider, LanguageProvider, ToastProvider, ErrorBoundary, SearchModal, global styles
  */
 
 import React from 'react'
@@ -12,6 +12,7 @@ import { ToastProvider } from '@/context/ToastContext'
 import { ToastContainer } from '@/components/UI/Toast/ToastContainer'
 import { ErrorBoundary } from '@/components/ErrorBoundary/ErrorBoundary'
 import { ShortcutsModal } from '@/components/KeyboardShortcuts/ShortcutsModal'
+import { SearchModal } from '@/components/Search/SearchModal'
 import '@/styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -28,6 +29,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
             <ToastContainer />
             <ShortcutsModal />
+            <SearchModal />
           </ToastProvider>
         </LanguageProvider>
       </ThemeProvider>
