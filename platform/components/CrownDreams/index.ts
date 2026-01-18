8f9da3e3943e7bd5ce75a5fbf5817cf6abb37767
Rtur2003 <hasannarthurrr@gmail.com>
2026-01-18 18:03:23 +0300

    feat: create index file for Crown Dreams components with exports for UI and 3D background elements

---
+++ platform/components/CrownDreams/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Crown Dreams Components
+ * Neural Dream Journal UI Components
+ * CrownCode Platform
+ */
+
+// UI Components
+export { GlassCard, StatCard, FeatureCard } from './GlassCard'
+export { ProgressBar, CircularProgress } from './ProgressBar'
+export { CyberButton, IconButton } from './CyberButton'
+
+// 3D Background
+export { default as GoldenParticles } from './GoldenParticles'
