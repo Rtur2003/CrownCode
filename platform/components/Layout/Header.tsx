1c24c566d6796e7015dc94ff0e6bbd34367d2cf3
Rtur2003 <hasannarthurrr@gmail.com>
2026-01-20 14:20:52 +0300

    feat: add 'commend' navigation item to the header

--- platform/components/Layout/Header.tsx
+++ platform/components/Layout/Header.tsx
@@ -30,6 +30,7 @@ export const Header: React.FC = () => {
     { label: t.nav.home, href: '/' },
     { label: t.nav.products, action: scrollToProducts },
     { label: t.nav.fortune, href: '/crown-fortune' },
+    { label: t.nav.commend, href: '/crown-commend' },
     { label: t.nav.github, href: 'https://github.com/Rtur2003?tab=repositories', external: true },
   ]
 
