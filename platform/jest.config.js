41a23b2925a53432e7ee629db8d7d917ecfd19cd
Rtur2003 <hasannarthurrr@gmail.com>
2026-03-03 10:13:32 +0300

    fix: correct setupFilesAfterEnv property in Jest configuration

--- platform/jest.config.js
+++ platform/jest.config.js
@@ -7,7 +7,7 @@ const createJestConfig = nextJest({
 /** @type {import('jest').Config} */
 const config = {
   testEnvironment: 'jsdom',
-  setupFilesAfterSetup: ['<rootDir>/jest.setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
